728c5dd86edb690fafea187c68433ed9fcddbc84
Akash kumar <akash8825317696@gmail.com>
2023-05-14 00:16:22 +0530

    commit 2 on 14 may 2023

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import { FaShoppingBag } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
+import { FaUserAlt } from 'react-icons/fa';
+import "./Navbar.css"
+function Navbar() {
+  return (
+    <>
+        <div className='Navbar__wrapper'>
+            <img className="Navbar__logo" src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/boAt_logo_small.svg?v=1682421543" alt="image"/>
+ 
+            <div>Categories</div>
+            <div>Daily Deals</div>
+            <div>Gift With boAt</div>
+            <div>More</div>
+      
+            <form className='form_wrapper'>
+              <input type="search" id="search-box"/>
+              <label htmlFor="search-box" className='search-box'><FaSearch/></label>
+            </form>
+          
+            <div><FaUserAlt/></div>
+
+            <div><FaShoppingBag/></div>
+            
+          
+            
+
+        </div>
+
+    </>
+  )
+}
+
+export default Navbar
